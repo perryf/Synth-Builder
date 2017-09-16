478870c48afffaf5ae3f9bd9723f6c5929bff470
Perry Fustero <pjfust@gmail.com>
2017-09-15 21:50:21 -0400

    fixed connection issue with lfo

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,7 @@ import './Main.css'
   })
 
   var lfoInit = new Tone.AutoFilter(200, 100, 2.6).start()
-  lfoInit.wet.value = 100
+  lfoInit.wet.value = 0
 
   var chorusInit = new Tone.Chorus(4, 2.5, 0.5)
   chorusInit.wet.value = 0
@@ -54,7 +54,7 @@ class Main extends Component {
       envelope: { attack: 0.01, decay: 0.1, sustain: 0.1, release: 0.1 }
     })
 
-    var newLfo = new Tone.AutoFilter()
+    var newLfo = new Tone.AutoFilter(200, 100, 2.6).start()
     newLfo.wet.value = 0
 
     var newChorus = new Tone.Chorus(9, 1);
@@ -67,7 +67,7 @@ class Main extends Component {
 
     this.setState({
       synths: [...this.state.synths, newSynth],
-      lfos: [...this.state.synths, newLfo],
+      lfos: [...this.state.lfos, newLfo],
       choruses: [...this.state.choruses, newChorus],
       distortions: [...this.state.distortions, newDist],
       colors: [...this.state.colors, newColor]
@@ -75,6 +75,7 @@ class Main extends Component {
   }
 
   render() {
+    console.log(this.state.lfos)
     let synths = this.state.synths.map((synth, index) => {
       let lfo = this.state.lfos[index]
       let chorus = this.state.choruses[index]
